a5a074edbd01ac124f505240c1b447e131ab4b94
Vladimir <JSdev.kuznetsov@yandex.ru>
2024-06-02 17:20:26 +0500

    Sem7 Task1 done

---
+++ Sem7Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("вВЕДИТЕ НАТУРАЛЬНОЕ ЧИСЛО:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void PrintOutNaturaleNumbers(int num)
+{
+    if (num == 0) return;
+    PrintOutNaturaleNumbers(num-1);
+    Console.Write($"{num} ");
+}
+
+PrintOutNaturaleNumbers(number);
