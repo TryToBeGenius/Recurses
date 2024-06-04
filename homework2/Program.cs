a034c990bff62b4169064d773eb5fad9be701edd
Vladimir <JSdev.kuznetsov@yandex.ru>
2024-06-04 16:01:46 +0500

    Zadacha2 reshena

---
+++ homework2/Program.cs
@@ -0,0 +1,12 @@
+﻿int Ack(int n,int m)
+{
+    if (n == 0) return m + 1;
+    if (m == 0) return Ack(n - 1, 1);
+    return Ack(n - 1, Ack(n , m - 1));
+}
+
+Console.WriteLine("Введите численное значение n");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите численное значение m");
+int m1 = Convert.ToInt32(Console.ReadLine());
+Console.Write(Ack(n1, m1));
