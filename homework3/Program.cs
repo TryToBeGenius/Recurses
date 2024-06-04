d59466a91ad05529e44744a5101006d204f51264
Vladimir <JSdev.kuznetsov@yandex.ru>
2024-06-04 18:33:59 +0500

    Zadacha3 Reshena

---
+++ homework3/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] arr = {1,2,5,0,10,34};
+void RecursForArray(int [] array1, int i = 0)
+{
+    if (i == array1.Length) return;
+    {
+       
+        RecursForArray(array1,i + 1);
+        Console.Write($"{array1[i]} ");
+        
+    }
+    
+
+}
+
+RecursForArray(arr, 0);
